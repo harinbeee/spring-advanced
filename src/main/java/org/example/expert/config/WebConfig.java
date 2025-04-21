bbaa11b709ae4ee28456da537d58167bc5a8e1ef
harinbeee <luv_lyn@naver.com>
2025-04-21 12:35:15 +0900

    Feat : Lv.4 인터셉터 등록

--- src/main/java/org/example/expert/config/WebConfig.java
+++ src/main/java/org/example/expert/config/WebConfig.java
@@ -1,8 +1,10 @@
 package org.example.expert.config;
 
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.common.adminlogging.AdminInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,4 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AuthUserArgumentResolver());
     }
+
+    // 인터셉터 등록
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminInterceptor())
+            .addPathPatterns("/admin/**"); // admin 접근시
+    }
+
 }
